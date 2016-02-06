626990c920b5d7fb7f4fb4fc3e82a62d99809521
Harish Koilada <rimcoharish@gmail.com>
2016-02-06 17:27:48 +0530

    Location finding done with numpy in RLGlue

--- pacman/pacman_image.py
+++ pacman/pacman_image.py
@@ -1,4 +1,6 @@
 from constants import *
+import numpy as np
+
 
 class pacman_image(object):
 	"""docstring for pacman_image"""
@@ -6,20 +8,11 @@ class pacman_image(object):
 		self.maze = maze
 
 	def detect_loc(self, screen, low, high):
-		row_index = 0
-		column_index = 0
-		total_matches = 0.0
-		row_sum = 0
-		column_sum = 0
-		for row in screen:
-			for color in row:
-				if color >= low and color <= high:
-					row_sum += row_index
-					column_sum += column_index
-					total_matches += 1
-				column_index += 1
-			row_index += 1
-		return (row_sum / total_matches, column_sum / total_matches)
+		indices = np.where(np.logical_and(low <= screen, screen <= high))
+		mean_pos = []
+		for coordinates in indices:
+			mean_pos.append(np.mean(coordinates))
+		return mean_pos
 
 	def detect_pacman_loc(self, screen):
 		return self.detect_loc(screen, PACMAN_LOW, PACMAN_HIGH)
